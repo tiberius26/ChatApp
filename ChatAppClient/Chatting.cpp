2512deb4f89358c9d3939d5872b7c9e3863c7f42
Tiberius26 <tibi_gabriel20@yahoo.com>
2020-11-11 20:23:35 +0000

    fixed some bugs

--- ChatAppClient/Chatting.cpp
+++ ChatAppClient/Chatting.cpp
@@ -3,12 +3,37 @@
 void Chatting::ChatLoop(TCPManager& ClientSide)
 {
 	M_ClientLocal = &ClientSide;
-	while (M_SentMessage != "end" && M_RecievedMessage != "end")
+	M_SendingThread = std::thread(&Chatting::Send, this);
+	M_ListeningThread = std::thread(&Chatting::Receive, this);
+	M_SendingThread.detach();
+	M_ListeningThread.join();
+}
+
+void Chatting::Receive()
+{
+	while (M_RecievedMessage != "end")
 	{
-		M_SendingThread = std::thread(&Chatting::Send, this);
-		M_ListeningThread = std::thread(&Chatting::Receive, this);
-		M_SendingThread.join();
-		M_ListeningThread.join();
+		if (M_ClientLocal->Receive(M_RecievedMessage))
+		{
+			std::cout << std::endl << "Received: " << M_RecievedMessage << std::endl;
+			//system("pause");
+		}
+		else { M_Tools->Debug("Can't recieve message", RED); }
+	}
+}
+
+void Chatting::Send()
+{
+	while (M_SentMessage != "end")
+	{
+		std::cout << "Say: ";
+		std::getline(std::cin, M_SentMessage);
+
+		if (!M_ClientLocal->Send(M_SentMessage))
+		{
+			M_Tools->Debug("Can't send message", RED);
+		}
+		if (M_SentMessage != "end") { M_SentMessage.clear(); }
 	}
 }
 
@@ -22,26 +47,4 @@ void Chatting::CloseChat()
 {
 	delete M_Tools;
 	M_ClientLocal = nullptr;
-}
-
-void Chatting::Receive()
-{
-	if (M_ClientLocal->Receive(M_RecievedMessage))
-	{
-		std::cout << std::endl << "Received: " << M_RecievedMessage << std::endl;
-		//system("pause");
-	}
-	else { M_Tools->Debug("Can't recieve message", RED); }
-}
-
-void Chatting::Send()
-{
-	std::cout << "Say: ";
-	std::getline(std::cin, M_SentMessage);
-
-	if (!M_ClientLocal->Send(M_SentMessage))
-	{
-		M_Tools->Debug("Can't send message", RED);
-	}
-	if (M_SentMessage != "end") { M_SentMessage.clear(); }
 }
